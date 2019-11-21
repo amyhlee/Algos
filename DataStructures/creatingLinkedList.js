cc37da17994e3a3617250eff3d70fd0cd379db43
Amy Lee <lee.asl@gmail.com>
2019-11-21 10:11:08 -0500

    implemented reverse method and saved

--- DataStructures/creatingLinkedList.js
+++ DataStructures/creatingLinkedList.js
@@ -55,6 +55,22 @@ class LinkedList {
     leader.next = nodeToRemove.next
     this.length--
   }
+  reverse() {
+    if(!this.head.next) {
+      return this.head
+    }
+    let first = this.head
+    let second = this.head.next
+    while(second) {
+      let temp = second.next
+      second.next = first
+      first = second
+      second = temp
+    }
+    this.head.next = null
+    this.head = first
+    return this
+  }
 }
 
 const myLinkedList = new LinkedList(10)
