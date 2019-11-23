0f1545646caad0cafb397a869f5acbdfa5dc0019
Amy Lee <lee.asl@gmail.com>
2019-11-23 11:44:25 -0500

    twoSumHash corrected variables

--- Algos/twoSumHash.js
+++ Algos/twoSumHash.js
@@ -1,12 +1,13 @@
-function twoSumHash(arr) {
+function twoSumHash(arr, target) {
   let cache = {}
 
   for (let i = 0; i < arr.length; i++) {
-    let diff = arr[i]
-    cache[diff] = i
+    let num = arr[i]
+    cache[num] = i
   }
 
   for (let i = 0; i < arr.length; i++) {
+    let diff = target - arr[i]
     if (cache.hasOwnProperty(diff) && cache[diff] !== i) {
       return [i, cache[diff]]
     }
