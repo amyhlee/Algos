ca7f758513f266d20294fa166eec1b09f2cf7be6
Amy Lee <lee.asl@gmail.com>
2019-11-29 10:40:03 -0500

    averagePair completed

---
+++ Algos/averagePair.js
@@ -0,0 +1,23 @@
+//Write a function called AveragePair. Given a sorted array of integers and the target average, determine ifthere is a pair of values in the array where the average of the pair equals the target average. There may be more than one pair that matches the average target
+
+function average(num1, num2) {
+  return Math.floor(num1 + num2/ 2)
+}
+
+function averagePair(arr, target) {
+  let start = 0
+  let end = arr.length - 1
+
+  while (start < end) {
+    let avg = average(arr[start], arr[end])
+
+    if (avg === target) {
+      return [arr[start], arr[end]]
+    } else if (avg > target) {
+      end--
+    } else {
+      start++
+    }
+  }
+  return 'not found'
+}
