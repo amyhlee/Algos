471c516e732bae66fa07f96790d43b0ab88ade38
Amy Lee <lee.asl@gmail.com>
2019-12-05 14:29:39 -0500

    factorial completed

---
+++ Algos/factorial.js
@@ -0,0 +1,9 @@
+// write a function that returns the factorial of a given parameter
+
+function factorial(number) {
+  let answer = 1
+  for (let i = 2; i <= number; i++) {
+    product *= i
+  }
+  return product
+}
