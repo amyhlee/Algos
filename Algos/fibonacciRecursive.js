334b34454e944773b6c45d49c3a76c7705a9e655
Amy Lee <lee.asl@gmail.com>
2019-12-06 09:48:30 -0500

    fibonacciRecursive: recursive solution implemented

--- Algos/fibonacciRecursive.js
+++ Algos/fibonacciRecursive.js
@@ -1,5 +1,8 @@
 // Given a number n, return the index value of the Fibonacci sequence
 
 function fibonacciRecursive(n) {
-
+  if (n < 2) {
+    return n
+  }
+  return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
 }
