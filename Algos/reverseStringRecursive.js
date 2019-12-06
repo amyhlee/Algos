de4b792abff97de41b1a43ebaa54cbe088232326
Amy Lee <lee.asl@gmail.com>
2019-12-06 14:22:08 -0500

    reverseStringRecursive: iterative method implemented

--- Algos/reverseStringRecursive.js
+++ Algos/reverseStringRecursive.js
@@ -1,3 +1,11 @@
 // reverse a given string
 //reverseString('yoyo mastery')  should return: 'yretsam oyoy'
 
+function reverseString(str) {
+  let reversedString = ''
+
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversedString += str[i]
+  }
+  return reversedString
+}
