288bddbf6a1fd87f71cd42f987decab882978e64
Amy Lee <lee.asl@gmail.com>
2019-12-06 09:55:57 -0500

    fibonacciRecursive: iterative approach implemented

--- Algos/fibonacciRecursive.js
+++ Algos/fibonacciRecursive.js
@@ -6,3 +6,11 @@ function fibonacciRecursive(n) {
   }
   return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2)
 }
+
+functon fibonacciIterative(n) {
+  let arr = [0 ,1]
+  for (let i = 2; i <= n; i++) {
+    arr.push(arr[i - 2] + arr[i - 1])
+  }
+  return arr[n]
+}
