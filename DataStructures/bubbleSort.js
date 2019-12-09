fdb967cdaa7cc45c2e37d853a2ffb0677a95cebe
Amy Lee <lee.asl@gmail.com>
2019-12-09 12:11:30 -0500

    bubbleSort: iterative solution implemented

---
+++ DataStructures/bubbleSort.js
@@ -0,0 +1,13 @@
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
+
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] > arr[j]) {
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
+      }
+    }
+  }
+}
