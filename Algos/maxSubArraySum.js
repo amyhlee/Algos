1d32bc9b1893c553b8d9e961a3351181a0b9682e
Amy Lee <lee.asl@gmail.com>
2019-12-14 18:09:54 -0500

    maxSubArraySum: optimized soln using sliding window implemented

--- Algos/maxSubArraySum.js
+++ Algos/maxSubArraySum.js
@@ -15,3 +15,19 @@ function maxSubArraySum(arr, n) {
   return max
 }
 
+function maxSubarraySum(arr, n) {
+  if (num > arr.length) return null
+
+  let max = 0
+  let temp = 0
+
+  for (let i = 0; i < num; i++) {
+    max += arr[i]
+  }
+  temp = max
+  for (let i = num; i < arr.length; i++) {
+    temp = temp - arr[i - num] + arr[i]
+    max = Math.max(temp, max)
+  }
+  return max
+}
