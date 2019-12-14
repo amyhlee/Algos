d030d8e8fb5bb2907b34f7367da1d8db2da3d403
Amy Lee <lee.asl@gmail.com>
2019-12-14 16:53:53 -0500

    maxSubArraySum: better naive approach implemented

--- Algos/maxSubArraySum.js
+++ Algos/maxSubArraySum.js
@@ -1,13 +1,12 @@
 function maxSubArraySum(arr, n) {
   let max = 0
 
-  for (let i = 0; i < arr.length; i++) {
-    let first = arr[i]
-    let second = arr[i + 1]
-
-    if (first + second > max) {
-      max = first + second
+  for (let i = 0; i < arr.length - n + 1; i++) {
+    let tempSum = 0
+    for (let j = 1; j < numbers; j++) {
+      tempSum += arr[i + j]
     }
+    max = Math.max(tempSum, max)
   }
   return max
 }
