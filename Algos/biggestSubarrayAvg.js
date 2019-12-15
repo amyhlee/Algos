02156abac291865034467e3a3610da75d9d15bba
Amy Lee <lee.asl@gmail.com>
2019-12-15 17:43:15 -0500

    biggestSubarrayAvg: sliding window implemented

--- Algos/biggestSubarrayAvg.js
+++ Algos/biggestSubarrayAvg.js
@@ -10,3 +10,21 @@ function biggestSubarrayAvg(arr, k) {
   }
   return Math.max(...results)
 }
+
+
+function biggestSubarrayAvgSlidingWindow(arr, k) {
+  let results = []
+  let start = 0
+  let sum = 0
+
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i]
+
+    if (i >= k - 1) {
+      results.push(sum / k)
+      sum -= arr[start]
+      start++
+    }
+  }
+  return Math.max(...results)
+}
