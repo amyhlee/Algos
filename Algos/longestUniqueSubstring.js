781acfdc06bd7378aa7e5b22da6bbe4cff5762be
Amy Lee <lee.asl@gmail.com>
2019-12-16 14:42:40 -0500

    longestUniqueSubstring: sliding window implemented

---
+++ Algos/longestUniqueSubstring.js
@@ -0,0 +1,26 @@
+function longestUniqueSubstring(str, k) {
+  let start = 0
+  let maxLength = 0
+  let cache = {}
+
+  for (let i = 0; i < str.length; i++) {
+    let rightChar = str[i]
+
+    if (!(rightChar in cache)) {
+      cache[rightChar] = 0
+    }
+    cache[rightChar]++
+
+    while(Object.keys(cache).length > k) {
+      let leftChar = str[start]
+      cache[leftChar] -= 1
+
+      if (cache[leftChar] === 0) {
+        delete cache[leftChar]
+      }
+      start++
+    }
+    maxLength = Math.max(maxLength, i - start + 1)
+  }
+  return maxLength
+}
