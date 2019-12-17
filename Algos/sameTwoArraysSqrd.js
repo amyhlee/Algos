e1720f00a10f082ddf7aeb8ab337c500b32a0891
Amy Lee <lee.asl@gmail.com>
2019-12-17 18:25:21 -0500

    sameTwoArraysSqrd: fixed typo in instruction

--- Algos/sameTwoArraysSqrd.js
+++ Algos/sameTwoArraysSqrd.js
@@ -1,4 +1,4 @@
-//Write a function called same which accepts two arrays. the function should return true if every value in the array has a corresponding value squared in the second array. the frequency of hte values must be the same.
+//Write a function called same which accepts two arrays. the function should return true if every value in the array has a corresponding value squared in the second array. the frequency of the values must be the same.
 
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
