063430221ec10a9a045488881fe5696911256c10
Amy Lee <lee.asl@gmail.com>
2019-12-17 11:42:55 -0500

    sameTwoArraysSqrd: slightly diff problem than sameTwoArrays and saved

---
+++ Algos/sameTwoArraysSqrd.js
@@ -0,0 +1,2 @@
+//Write a function called same which accepts two arrays. the function should return true if every value in the array has a corresponding value squared in the second array. the frequency of hte values must be the same.
+
