cbecfbf528009461eb3da347777163cfeaf5cc53
Amy Lee <lee.asl@gmail.com>
2019-12-20 21:35:45 -0500

    anagramSliding: sliding window rough draft

--- Algos/anagramSliding.js
+++ Algos/anagramSliding.js
@@ -3,4 +3,43 @@ function anagramSliding(str, pattern) {
   let matched = 0
   let cache = {}
   let results = []
+
+  for (let i = 0; i < pattern.length; i++) {
+    let letter = pattern[i]
+
+    if (!(letter in cache)) {
+      cache[letter] = 1
+    } else {
+      cache[letter]++
+    }
+  }
+
+  for (let j = 0; j < str.length; j++) {
+    let right = str[j]
+
+    if (right in cache) {
+      cache[right]--
+      if (cache[right] === 0) {
+        matched++
+      }
+    }
+
+    if (matched === Object.keys(cache).length) {
+      results.push(start)
+    }
+
+    if (j >= pattern.length - 1) {
+      let left = str[start]
+      start++
+
+      if (left in cache) {
+        cache[left]--
+        if (cache[left] === 0) {
+          matched--
+        }
+        cache[left]++
+      }
+    }
+  }
+  return results
 }
