3d142c301edc434bcd6b14ab16252cbceb32d5cd
Amy Lee <lee.asl@gmail.com>
2019-12-31 18:19:39 -0500

    pairSum: naive approach implemented

---
+++ Algos/pairSumTwoPointer.js
@@ -0,0 +1,10 @@
+function pairSum(arr, target) {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === target) {
+        return [arr[i], arr[j]]
+      }
+    }
+  }
+}
+
--- Algos/twoSumHash.js
+++ Algos/twoSumHash.js
@@ -13,3 +13,4 @@ function twoSumHash(arr, target) {
     }
   }
 }
+
