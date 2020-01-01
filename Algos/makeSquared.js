bef8ac3829ad540543536560c503c0866417cc90
Amy Lee <lee.asl@gmail.com>
2020-01-01 18:46:28 -0500

    makeSquared: optimized implemented

---
+++ Algos/makeSquared.js
@@ -0,0 +1,24 @@
+// Given a sorted array, create a new array containing squares of all the number of the input array in the sorted order.
+
+function makeSquared(arr) {
+  let n = arr.length
+  let squaredArr = Array(n).fill(0)
+  let highestIndex = n - 1
+  let left = 0
+  let right = n- 1
+
+  while(left <= right) {
+    let leftSquared = arr[left] * arr[left]
+    let rightSquared = arr[right] * arr[right]
+
+    if (leftSquared > rightSquared) {
+      squaredArr[highestIndex] = leftSquared
+      left += 1
+    } else {
+      squaredArr[highestIndex] = rightSquared
+      right -= 1
+    }
+    highestIndex += 1
+  }
+  return squaredArr
+}
