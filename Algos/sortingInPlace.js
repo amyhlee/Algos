783a9fc581f930ac3ecd49c8f8b40fba2b66f4b7
Amy Lee <lee.asl@gmail.com>
2020-01-03 21:20:54 -0500

    sortingInPlace: solution implemented

---
+++ Algos/sortingInPlace.js
@@ -0,0 +1,17 @@
+function sortingInPlace(arr) {
+  let i = 0
+  let low = 0
+  let high = arr.length
+
+  while(i <= high) {
+    if (arr[i] === 0) {
+      [arr[i], arr[low]] = [arr[low], arr[i]]
+      i++
+      low++
+    } else if (arr[i] === 1) {
+      i++
+    } else {
+      [arr[i], arr[high]] = [arr[high], arr[i]]
+    }
+  }
+}
