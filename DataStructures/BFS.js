505061d12763da95f64be1769f3f0f4fc12864cd
Amy Lee <lee.asl@gmail.com>
2020-01-15 15:44:16 -0500

    BFS: maxDepth implemented

--- DataStructures/BFS.js
+++ DataStructures/BFS.js
@@ -70,3 +70,25 @@ function minimumDepth(root) {
     }
   }
 }
+
+function maximumDepth(root) {
+  let queue = []
+
+  if (root === null) return 0
+
+  queue.push(root)
+  let maximumDepth = 0
+  while (queue.length) {
+    maximumDepth++
+    let currentLevel = []
+    let levelSize = queue.length
+    for (let i = 0; i < levelSize; i++) {
+      let currentNode = queue.shift()
+      currentLevel.push(currentNode.val)
+
+      if (currentNode.left) queue.push(currentNode.left)
+      if (currentNode.right) queue.push(currentNode.right)
+    }
+  }
+  return maximumDepth
+}
