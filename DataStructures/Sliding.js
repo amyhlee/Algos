91ebbd156b92c9ff5758544179e56ba2ae60cff8
Amy Lee <lee.asl@gmail.com>
2020-01-17 15:00:40 -0500

    Sliding: longestSubstringOfK

--- DataStructures/Sliding.js
+++ DataStructures/Sliding.js
@@ -33,3 +33,28 @@ start++
 // outside for loop, place edge case
 if (minLength === Infinity) return 0
 // otherwise return minLength
+
+
+// longest substring with no more than k distinct characters
+
+//Input: String="araaci", K=2
+// Output: 4
+// Explanation: The longest substring with no more than '2' distinct characters is "araa".
+
+let start = 0
+let maxLength = 0
+let cache = {}
+
+//iterate over string
+//place letters into cache up to k
+//in for loop, while loop
+while (Object.keys(cache).length > k)
+//start narrowing window
+let leftChar = str[start]
+cache[leftChar]--
+if (cache[leftChar] === 0) delete cache[leftChar]
+//move window forward
+start++
+//determine maxLength
+maxLength = Math.max(maxLength, i - start + 1)
+//return maxLength
