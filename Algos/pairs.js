38466877d71273ecad01290fbaf98f2153db7217
Amy Lee <lee.asl@gmail.com>
2020-01-16 20:38:29 -0500

    Pairs: optimized soln with hash

--- Algos/pairs.js
+++ Algos/pairs.js
@@ -1,21 +1,20 @@
 // You will be given an array of integers and a target value. Determine the number of pairs of array elements that have a difference equal to a target value.
 
 function pairs(arr, k) {
-  arr = arr.sort()
-  let i = 0
-  let j = i + 1
+  let hash = {}
   let count = 0
-
-  while (j <= arr.length - 1) {
-    let diff = arr[j] - arr[i]
-
-    if (diff === k) {
+  for (let i = 0; i < arr.length; i++) {
+    let value = arr[i]
+    if (!(value in hash)) {
+      hash[value] = 1
+    } else {
+      hash[value]++
+    }
+  }
+  for (let j = 0; j < arr.length; j++) {
+    let diff = arr[j] - k
+    if (diff in hash) {
       count++
-      j++
-    } else if (diff < k) {
-      j++
-    } else if (diff > k) {
-      i++
     }
   }
   return count
