a116e76c213145b8de5308c99001f305c633f155
Amy Lee <lee.asl@gmail.com>
2020-01-17 10:00:45 -0500

    Sliding: minLengthOfK notes

--- DataStructures/Sliding.js
+++ DataStructures/Sliding.js
@@ -16,4 +16,20 @@ start++
 //return maxSum
 
 
-//
+//smallest subarray of k
+let windowSum = 0
+let start = 0
+let minLength = Infinity
+
+//iterate over array
+
+while (windowSum >= k)
+minLength = Math.min(minLength, i - start + 1)
+//shrink window
+windowSum -= arr[start]
+//slide window forward
+start++
+
+// outside for loop, place edge case
+if (minLength === Infinity) return 0
+// otherwise return minLength
