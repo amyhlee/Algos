b83746c4a334a1cd3f888401d800e463d6acaa5c
Amy Lee <lee.asl@gmail.com>
2020-02-05 08:33:22 -0500

    closure problem

--- Concepts/closures.js
+++ Concepts/closures.js
@@ -10,6 +10,9 @@ const add5 = partial(add ,5)
 
 add5(4) // 9
 
+//partial accepts two arguments, function and number
+//partial must return a function, because we're assigning its return value to add5 and then calling add5()
+
 function partial(fn, num) {
   return function(b) {
     return fn(num, b)
