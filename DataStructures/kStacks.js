c56ac2a30b1905eb8f62d95de8c3bcc6789b8603
Amy Lee <lee.asl@gmail.com>
2020-02-06 17:20:04 -0500

    kStacks constructor

---
+++ DataStructures/kStacks.js
@@ -0,0 +1,13 @@
+/* Create a data structure KStacks that represents a set of k stacks. It should only use one array. The following methods must be supported by KStacks.
+
+push(val, stackNumber) –> pushes x to stack number ‘sn’ where sn is from 0 to k-1
+pop(stackNumber) –> pops an element from stack number ‘sn’ where sn is from 0 to k-1
+*/
+
+class KStacks {
+  constructor(size) {
+    this._storage = []
+    this._lengths = new Array(size).fill(0)
+  }
+
+}
