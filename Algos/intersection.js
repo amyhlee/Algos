6caa2a934f47b0a6565a731ae7edddb54e7ff73a
Amy Lee <lee.asl@gmail.com>
2020-02-11 20:12:25 -0500

    intersect prompt added

--- Algos/intersection.js
+++ Algos/intersection.js
@@ -1,3 +1,12 @@
+//Given two arrays, write a function to compute their intersection.
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [4,9]
+
+
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2]
+
+
 function intersect(arr1, arr2) {
   let firstSet = new Set(arr1)
   let secondSet = new Set()
