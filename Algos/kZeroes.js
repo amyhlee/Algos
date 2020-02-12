9aabaf114c2e5052965ae611e68907c9aec1d68a
Amy Lee <lee.asl@gmail.com>
2020-02-12 18:30:41 -0500

    kZeroes soln

---
+++ Algos/kZeroes.js
@@ -0,0 +1,19 @@
+//Given an array A of 0s and 1s, we may change up to K values from 0 to 1.
+
+// Return the length of the longest (contiguous) subarray that contains only 1s.
+
+function kZeroes(arr, k) {
+  let left = 0
+  let right;
+  let counter = 0
+
+  for (right = 0; right < arr.length; right++) {
+    if (arr[right] == 0) counter--
+
+    if (counter > 0) {
+      left++
+      counter++
+    }
+  }
+  return right - left
+}
