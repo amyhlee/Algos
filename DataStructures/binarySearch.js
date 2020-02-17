31838e345290f160b62d604a40e0e557910dac4d
Amy Lee <lee.asl@gmail.com>
2020-02-17 17:42:09 -0500

    findRange: binary search implemented

--- DataStructures/binarySearch.js
+++ DataStructures/binarySearch.js
@@ -23,3 +23,40 @@ function nextGreatest(arr, key) {
   }
   return arr[start % arr.length]
 }
+
+//Given an array of numbers sorted in ascending order, find the range of a given number ‘key’. The range of the ‘key’ will be the first and last position of the ‘key’ in the array.
+
+// Write a function to return the range of the ‘key’. If the ‘key’ is not present return [-1, -1].
+
+
+function range(arr, key) {
+  let result = [-1, -1]
+  result[0] = binarySearch(arr, key, false)
+  if (result[0] !== -1) {
+    result[1] = binarySearch(arr, key, true)
+  }
+}
+
+function binarySearch(arr, key, findMaxIndex) {
+  let keyIndex = -1
+  let start = 0
+  let end = arr.length - 1
+
+  while (start <= end) {
+    let mid = Math.floor(start + (end-start) / 2)
+
+    if (key < arr[mid]) {
+      end = mid - 1
+    } else if (key > arr[mid]) {
+      start = mid + 1
+    } else {
+      keyIndex = mid
+      if (findMaxIndex) {
+        start = mid + 1
+      } else {
+        end = mid - 1
+      }
+    }
+  }
+  return keyIndex
+}
