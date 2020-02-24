7925d57ddc558a166a864508eebe1f0f043c2581
Amy Lee <lee.asl@gmail.com>
2020-02-24 08:09:41 -0500

    generate: soln implemented

--- Algos/validParentheses.js
+++ Algos/validParentheses.js
@@ -16,3 +16,46 @@ function valid(str) {
   if (str.length === 0) return true
   return false
 }
+
+//return how many parentheses need to be added to balance
+
+function balance(str) {
+  let balanced = 0
+  let answer = 0
+
+  for (let i = 0; i < str.length; i++) {
+    let char = str[i]
+
+    if (char == '(') {
+      balanced++
+    } else {
+      balanced--
+    }
+
+    if (balanced === -1) {
+      answer++
+      balanced++
+    }
+  }
+  return Math.abs(answer + balanced)
+}
+
+//generate parentheses
+function generate(num) {
+  let results = []
+
+  function build(leftCount, rightCount, partial) {
+    if (leftCount > rightCount) return
+    if (leftCount === 0 && rightCount === 0) {
+      results.push(partial)
+    }
+    if (leftCount > 0) {
+      build(leftCount - 1, rightCount, partial += '(')
+    }
+    if (rightCount > 0) {
+      build(leftCount, rightCount - 1, partial += ')')
+    }
+  }
+  build(num, num, partial)
+  return results
+}
