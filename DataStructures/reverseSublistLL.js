80aff579d08a5bad3f61e50a5747352e2b54a85e
Amy Lee <lee.asl@gmail.com>
2020-03-03 15:41:35 -0500

    reverseSublistLL: soln implemented

---
+++ DataStructures/reverseSublistLL.js
@@ -0,0 +1,38 @@
+class Node {
+  constructor(value) {
+    this.value = value
+    this.next = null
+  }
+}
+
+function reverseSublist(head, p, q) {
+  let curr = head
+  let prev = null
+  let i = 0
+
+  while (curr !== null && i < p - 1) {
+    prev = curr
+    curr = curr.next
+    i++
+  }
+
+  let lastNodeFirstPart = prev
+  let lastNodeSublist = curr
+  i = 0
+
+  while (curr !== null && i < q - p + 1) {
+    next = curr.next
+    curr.next = prev
+    prev = curr
+    curr = next
+    i++
+  }
+
+  if (lastNodeFirstPart !== null) {
+    lastNodeFirstPart.next = prev
+  } else {
+    head = prev
+  }
+  lastNodeSublist.next = curr
+  return head
+}
