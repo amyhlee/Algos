57df0432f7332c107b6a7c39e5a7ec704c164176
Amy Lee <lee.asl@gmail.com>
2020-03-03 15:51:38 -0500

    reverseSublistLL: soln implemented for reversing k elements

--- DataStructures/reverseSublistLL.js
+++ DataStructures/reverseSublistLL.js
@@ -36,3 +36,38 @@ function reverseSublist(head, p, q) {
   lastNodeSublist.next = curr
   return head
 }
+
+function reverseByK(head, k) {
+  if (k <= 1 || head === null) return head
+
+  let curr = head
+  let prev = null
+
+  while (true) {
+
+    let lastNodeFirstPart = prev
+    let lastNodeSublist = curr
+    let i = 0
+
+    while (curr !== null && i < k) {
+      next = curr.next
+      curr.next = prev
+      prev = curr
+      curr = next
+      i++
+    }
+
+    if (lastNodeFirstPart !== null) {
+      lastNodeFirstPart.next = prev
+    } else {
+      head = prev
+    }
+    lastNodeSublist.next = curr
+
+    if (curr === null) {
+      break
+    }
+    prev = lastNodeSublist
+  }
+  return head
+}
