59f0f6d2cdfe78e4882cc05e1b63d543c95ab735
Amy Lee <lee.asl@gmail.com>
2020-03-09 19:45:39 -0400

    findPaths soln implemented

--- DataStructures/DFS.js
+++ DataStructures/DFS.js
@@ -9,3 +9,26 @@ function findSum(root, sum) {
 
   return findSum(root.left, sum - root.val) || findSum(root.right, sum - root.val)
 }
+
+// return all paths that equals sum from root to leaf
+
+function findPaths(root, sum) {
+  let allPaths = []
+
+  findPathRecursive(currentNode, sum, [], allPaths)
+  return allPaths
+}
+
+function findPathRecursive(currentNode, sum, currentPath, allPaths) {
+  if (root === null) return
+
+  currentPath.push(currentNode.value)
+
+  if (currentNode.value === sum && currentNode.left === null && currentNode.right === null) {
+    allPaths.push(currentPath)
+  } else {
+    findPathRecursive(currentNode.left, sum - currentNode.value, currentPath, allPaths)
+    findPathRecursice(currentNode.right, sum - currentNode.value, currentPath, allPaths)
+  }
+  currentPath.pop()
+}
