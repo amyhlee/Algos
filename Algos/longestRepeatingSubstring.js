ab51e420018a15a01c87dc3fb88a6656dbc59179
Amy Lee <lee.asl@gmail.com>
2020-03-13 14:40:23 -0400

    substitutingK soln implemented

--- Algos/longestRepeatingSubstring.js
+++ Algos/longestRepeatingSubstring.js
@@ -13,3 +13,32 @@ function longestRepeating(str) {
   }
   return maxLength
 }
+
+// find the longest repeating substring that allows for k substitutions
+
+function substitutingK(str, k) {
+  let hash = {}
+  let start = 0
+  let maxLength = 0
+  let maxRepeating = 0
+
+  for (let i = 0; i < str.length; i++) {
+    let rightChar = str[i]
+
+    if (!(rightChar in hash)) {
+      hash[rightChar] = 1
+    } else {
+      hash[rightChar]++
+    }
+
+    maxRepeating = Math.max(maxRepeating, hash[rightChar])
+
+    if (i - start + 1 - maxRepeating > k) {
+      let leftChar = str[start]
+      hash[leftChar] -= 1
+      start += 1
+    }
+    maxLength = Math.max(maxLength, i - start + 1)
+  }
+  return maxLength
+}
