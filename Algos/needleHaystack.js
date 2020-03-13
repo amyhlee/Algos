00d1ce31409b84d5fabe5eebec5c37647ec68e3d
Amy Lee <lee.asl@gmail.com>
2020-03-13 19:31:30 -0400

    search soln implemented

---
+++ Algos/needleHaystack.js
@@ -0,0 +1,14 @@
+// Return the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+// haystack = "hello", needle = "ll"
+
+function search(haystack, needle) {
+  if (haystack === needle || needle.length === 0) return 0
+
+  for (let i = 0; i < haystack.length; i++) {
+    if (haystack[i] === needle[0]) {
+      let temp = haystack.substring(i, i + needle.length)
+      if (temp === needle.length) return i
+    }
+  }
+  return -1
+}
