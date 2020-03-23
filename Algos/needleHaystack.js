36c2e09a713b5561c05016897fde82136df7e1b1
Amy Lee <lee.asl@gmail.com>
2020-03-22 22:10:33 -0400

    needleHaystack: fixed line 10

--- Algos/needleHaystack.js
+++ Algos/needleHaystack.js
@@ -7,7 +7,7 @@ function search(haystack, needle) {
   for (let i = 0; i < haystack.length; i++) {
     if (haystack[i] === needle[0]) {
       let temp = haystack.substring(i, i + needle.length)
-      if (temp === needle.length) return i
+      if (temp === needle) return i
     }
   }
   return -1
