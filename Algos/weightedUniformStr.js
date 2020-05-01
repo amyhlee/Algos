3c2b4809914bdf65f9363c152f16e4d968354529
Amy Lee <lee.asl@gmail.com>
2020-05-01 14:17:03 -0400

    weightedUniformStr soln added

---
+++ Algos/weightedUniformStr.js
@@ -0,0 +1,36 @@
+//Given a string and an array of queries, return an array that states if the query can be found in the given string
+
+//Example:
+////'aaaddeff' --> [ 'Yes', 'Yes', 'No', 'No', 'No' ]
+
+function weights(str, queries) {
+  let map = {}
+  let value = 1
+
+  for (let i = 0; i < alphabet.length; i++) {
+    map[alphabet[i]] = value
+    value++
+  }
+  let count = 1
+  let weighted = []
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i+1]) {
+      count++
+    }else if (count === 1) {
+      weighted.push(map[str[i]])
+    }else{
+      weighted.push(map[str[i]] * count)
+      count = 1
+    }
+  }
+  let result = []
+  for (let i = 0; i < queries.length; i++) {
+    if (weighted.indexOf(queries[i]) !== -1) {
+      result.push('Yes')
+    }else{
+      result.push('No')
+    }
+  }
+  return result
+}
