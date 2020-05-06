b1eef5c4789293823b8bd8f5ab1a48d369914f48
Amy Lee <lee.asl@gmail.com>
2020-05-06 17:55:42 -0400

    sellStock soln added

---
+++ Algos/sellStock.js
@@ -0,0 +1,18 @@
+//Say you have an array for which the ith element is the price of a given stock on day i.
+
+// If you were only permitted to complete at most one transaction (i.e., buy one and sell one share of the stock), design an algorithm to find the maximum profit.
+
+// Note that you cannot sell a stock before you buy one.
+
+function maxProfit(prices) {
+  let maxProfit = 0
+  let minBuyPrice = Infinity
+
+  for (let i = 0; i < prices.length; i++) {
+    if (prices[i] < minBuyPrice) {
+      minBuyPrice = prices[i]
+    }
+    maxProfit = Math.max(maxProfit, prices[i] - minBuyPrice)
+  }
+  return maxProfit
+}
