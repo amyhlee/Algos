c020e42158f17a33212cdd858ab324fb0279ee1f
Amy Lee <lee.asl@gmail.com>
2020-05-12 18:31:11 -0400

    prodInArr soln added

---
+++ Algos/prodInArr.js
@@ -0,0 +1,16 @@
+//Given an array nums of n integers where n > 1,  return an array output such that output[i] is equal to the product of all the elements of nums except nums[i].
+
+function prodInArr(arr) {
+  let result = []
+  result[0] = 1
+
+  for (let i = 1; i < arr.length; i++) {
+    result[i] = result[i-1] * arr[i-1]
+  }
+  let right = 1
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result[i] *= right
+    right *= arr[i]
+  }
+  return result
+}
