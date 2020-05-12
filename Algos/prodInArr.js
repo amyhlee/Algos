12d2e92c36a46f3082b7a2dc4e510aca4ff77a05
Amy Lee <lee.asl@gmail.com>
2020-05-12 18:38:24 -0400

    prodInArr saved

--- Algos/prodInArr.js
+++ Algos/prodInArr.js
@@ -4,9 +4,12 @@ function prodInArr(arr) {
   let result = []
   result[0] = 1
 
+  // everything to the right
   for (let i = 1; i < arr.length; i++) {
     result[i] = result[i-1] * arr[i-1]
   }
+
+  // everything to the left
   let right = 1
   for (let i = arr.length - 1; i >= 0; i--) {
     result[i] *= right
