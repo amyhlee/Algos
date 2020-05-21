391489f8b9e857ec440ae104ffb6f75e7bc93615
Amy Lee <lee.asl@gmail.com>
2020-05-21 10:11:39 -0400

    whileLoop notes added

---
+++ Concepts/whileLoop.js
@@ -0,0 +1,5 @@
+//Notes
+
+//You use while (start <= end) if you are returning the match from inside the loop.
+
+// You use while (start < end) if you want to exit out of the loop first, and then use the result of start or end to return the match.
