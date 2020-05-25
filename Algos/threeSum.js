cb3fbf529d4f4ace3c092ae6524a7d2d74cd52dd
Amy Lee <lee.asl@gmail.com>
2020-05-25 12:34:12 -0400

    3Sum soln added

--- Algos/threeSum.js
+++ Algos/threeSum.js
@@ -74,3 +74,40 @@ function threeSum(nums, target) {
 
 	return results
 };
+
+
+function threeSum(nums, target) {
+	const results = []
+
+	if (nums.length < 3) return results
+
+	nums = nums.sort((a, b) => a - b)
+
+
+	for (let i = 0; i < nums.length - 2; i++) {
+		if (nums[i] > target) break
+		if (i > 0 && nums[i] === nums[i - 1]) continue
+
+		let j = i + 1
+		let k = nums.length - 1
+
+		while (j < k) {
+			let sum = nums[i] + nums[j] + nums[k]
+
+			if (sum === target) {
+				results.push([nums[i], nums[j], nums[k]])
+
+				while (nums[j] === nums[j + 1]) j++
+				while (nums[k] === nums[k - 1]) k--
+
+				j++
+				k--
+			} else if (sum < target) {
+				j++
+			} else { // (sum > target)
+				k--
+			}
+		}
+	}
+	return results
+};
