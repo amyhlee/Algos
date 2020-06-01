c3058e60497ba37a7a5017cd5b2da18aa3759ce1
Amy Lee <lee.asl@gmail.com>
2020-06-01 10:42:43 -0400

    removeVowel .match regex soln added

--- Concepts/strings.js
+++ Concepts/strings.js
@@ -9,3 +9,12 @@ function removeConsonants(str) {
   str = str.replace(/[^aeiou]/gi, '')
   return str     //'basket' --> 'ae'
 }
+
+
+// .match with regex
+
+function removeVowel(str) {
+  const regex = /[^aeiou]/gi
+  const result = str.match(regex)
+  return result.join('')  //'basket' --> 'bskt'
+}
