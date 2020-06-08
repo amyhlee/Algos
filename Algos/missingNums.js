b165b6feb04a87433f090991e0c58b9fe91ad7e1
Amy Lee <lee.asl@gmail.com>
2020-06-08 11:18:42 -0400

    missingNums soln added

---
+++ Algos/missingNums.js
@@ -0,0 +1,16 @@
+//given an array, return an array that contains the numbers to create an array of consecutive numbers
+
+function missingNums(arr) {
+  let result = []
+
+  for (let i = 0; i < arr.length - 1; i++) {
+    let val = arr[i] + 1
+
+    if (arr.indexOf(val) == -1) {
+      result.push(val)
+    }
+  }
+  return result
+}
+
+disa([1, 3, 4, 6])
