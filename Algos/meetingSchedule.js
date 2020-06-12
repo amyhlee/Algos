c7a8a3f6f71a06e5a2e01f18208c3dfa88cdaea8
Amy Lee <lee.asl@gmail.com>
2020-06-12 11:40:25 -0400

    minTime soln added

--- Algos/meetingSchedule.js
+++ Algos/meetingSchedule.js
@@ -69,3 +69,24 @@ function homework(startTime, endTime, queryTime) {
   }
   return count
 }
+
+
+//On a plane there are n points with integer coordinates points[i] = [xi, yi]. Your task is to find the minimum time in seconds to visit all points.
+//Input: points = [[1,1],[3,4],[-1,0]]
+// Output: 7
+// Explanation: One optimal path is [1,1] -> [2,2] -> [3,3] -> [3,4] -> [2,3] -> [1,2] -> [0,1] -> [-1,0]
+// Time from [1,1] to [3,4] = 3 seconds
+// Time from [3,4] to [-1,0] = 4 seconds
+// Total time = 7 seconds
+
+function minTime(points) {
+  let time = 0
+
+  for (let i = 0; i < points.length - 1; i++) {
+    let x = Math.abs(points[i+1][0] - points[i][0])
+    let y = Math.abs(points[i+1][1] - points[i][1])
+
+    time += Math.max(x, y)
+  }
+  return time
+}
